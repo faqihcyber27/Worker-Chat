b718eba9b56f611cd5f5e6b15bddfb957b14d902
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2026-03-26 19:26:39 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -64,6 +64,11 @@ export class ChatRoom {
   // ================= 🔥 ONLINE =================
   if (data.type === "online") {
     const now = new Date().toISOString()
+
+  // 🔥 update last seen tiap ping
+  await this.env.DB.prepare(`
+    UPDATE users SET last_seen = ? WHERE email = ?
+  `).bind(now, data.user).run()
   
   const payload = {
     type: "online",
