89e922baf44e478aaedc7c22d2736f4d40b974a0
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2026-03-28 15:24:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,21 @@
-if (url.pathname === "/sw.js") {
-  return new Response(`
-    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js')
-    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js')
 
-    firebase.initializeApp({
-      apiKey: "AIzaSyCASPxiCM8V8OIzw3JaeTdGreID0EMyTMk",
-      authDomain: "chat-realtime-7b092.firebaseapp.com",
-      projectId: "chat-realtime-7b092",
-      messagingSenderId: "424831780632",
-      appId: "1:424831780632:web:6e42d806ea94392778406d"
-    })
+firebase.initializeApp({
+  apiKey: "AIzaSyCASPxiCM8V8OIzw3JaeTdGreID0EMyTMk",
+  authDomain: "chat-realtime-7b092.firebaseapp.com",
+  projectId: "chat-realtime-7b092",
+  messagingSenderId: "424831780632",
+  appId: "1:424831780632:web:6e42d806ea94392778406d"
+})
 
-    const messaging = firebase.messaging()
+const messaging = firebase.messaging()
 
-    messaging.onBackgroundMessage(function(payload) {
-      self.registration.showNotification(payload.notification.title, {
-        body: payload.notification.body
-      })
-    })
-
-    self.addEventListener("install", e => self.skipWaiting())
-    self.addEventListener("activate", e => self.clients.claim())
-  `, {
-    headers: { "Content-Type": "application/javascript" }
+messaging.onBackgroundMessage((payload)=>{
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body
   })
-}
+})
+
+self.addEventListener("install", e => self.skipWaiting())
+self.addEventListener("activate", e => self.clients.claim())
