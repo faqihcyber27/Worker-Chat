3ef77d5d81e1325ff99200b48d1cb13fcb715030
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2026-03-28 07:24:57 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -527,6 +527,34 @@ if (url.pathname === "/login" && request.method === "POST") {
     })
   }
   
+  if (url.pathname === "/sw.js") {
+  return new Response(`
+    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js')
+    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js')
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyCASPxiCM8V8OIzw3JaeTdGreID0EMyTMk",
+      authDomain: "chat-realtime-7b092.firebaseapp.com",
+      projectId: "chat-realtime-7b092",
+      messagingSenderId: "424831780632",
+      appId: "1:424831780632:web:6e42d806ea94392778406d"
+    })
+
+    const messaging = firebase.messaging()
+
+    messaging.onBackgroundMessage(function(payload) {
+      self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body
+      })
+    })
+
+    self.addEventListener("install", e => self.skipWaiting())
+    self.addEventListener("activate", e => self.clients.claim())
+  `, {
+    headers: { "Content-Type": "application/javascript" }
+  })
+}
+  
   if (url.pathname === "/manifest.json") {
   return new Response(JSON.stringify({
     name: "Realtime Chat",
