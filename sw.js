2574cdfeaebc530941bc157d8305185215822303
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2026-03-28 07:27:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
-self.addEventListener("push", event => {
-  const data = event.data.json()
+if (url.pathname === "/sw.js") {
+  return new Response(`
+    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js')
+    importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js')
 
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png",
-    data: data.url
+    firebase.initializeApp({
+      apiKey: "AIzaSyCASPxiCM8V8OIzw3JaeTdGreID0EMyTMk",
+      authDomain: "chat-realtime-7b092.firebaseapp.com",
+      projectId: "chat-realtime-7b092",
+      messagingSenderId: "424831780632",
+      appId: "1:424831780632:web:6e42d806ea94392778406d"
+    })
+
+    const messaging = firebase.messaging()
+
+    messaging.onBackgroundMessage(function(payload) {
+      self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body
+      })
+    })
+
+    self.addEventListener("install", e => self.skipWaiting())
+    self.addEventListener("activate", e => self.clients.claim())
+  `, {
+    headers: { "Content-Type": "application/javascript" }
   })
-})
-
-self.addEventListener("notificationclick", event => {
-  event.notification.close()
-
-  event.waitUntil(
-    clients.openWindow(event.notification.data)
-  )
-})
+}
