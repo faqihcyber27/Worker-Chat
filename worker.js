8c09651be80cc7022246f7535ef1d68412a6d838
faqihcyber27 <34843362+faqihcyber27@users.noreply.github.com>
2026-03-29 05:03:00 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -417,6 +417,60 @@ export default {
   async fetch(request, env) {
 
     const url = new URL(request.url)
+    
+    if (url.pathname === "/register" && request.method === "POST") {
+        const { name, email, password } = await request.json()
+        const cleanEmail = email.trim().toLowerCase()
+
+        const exist = await env.DB.prepare(`
+          SELECT email FROM users WHERE email=?
+        `).bind(cleanEmail).first()
+
+        if (exist) {
+          return json({ error: "Email sudah terdaftar" })
+        }
+
+        const hash = await hashPassword(password)
+
+        await env.DB.prepare(`
+          INSERT INTO users (name,email,password)
+          VALUES (?,?,?)
+        `).bind(name, cleanEmail, hash).run()
+
+        return json({
+        token:"ok",
+        user:{ name, email:cleanEmail }
+      })
+    }
+    
+    if (url.pathname === "/login" && request.method === "POST") {
+
+        const { email, password } = await request.json()
+        const cleanEmail = email.trim().toLowerCase()
+        const user = await env.DB.prepare(`
+          SELECT * FROM users WHERE email=?
+        `).bind(cleanEmail).first()
+
+        if (!user) {
+          return json({ error:"Email tidak ditemukan" })
+        }
+
+        const hash = await hashPassword(password)
+
+        if (user.password !== hash) {
+          return json({ error:"Password salah" })
+        }
+
+        return json({
+          token:"ok",
+          user:{
+          email:user.email,
+          name:user.name,
+          avatar:user.avatar,
+          bio:user.bio
+        }
+      })
+    }
 
     if (url.pathname === "/ws") {
       const room = url.searchParams.get("room")
